fc9280aa89319a66f4c72faeede7ada31c26df38
leojia22 <ljia2@zagmail.gonzaga.edu>
2024-11-19 09:02:32 -0800

    initial

---
+++ interview_client.py
@@ -0,0 +1,16 @@
+import requests # a lib for making http requests
+import json # a lib fofr working with json
+
+url = "https://flask-app-demo.onrender.com/predict?level=Junior&lang=Java&tweets=yes&phd=no"
+
+response = requests.get(url=url)
+
+# first thing, check the response's status code
+print(response.status_code)
+if response.status_code == 200:
+    # STATUS OK
+    # we can extract the prediction from the response's JSON text
+    json_object = json.loads(response.text)
+    print(json_object)
+    pred = json_object["prediction"]
+    print("prediction:", pred)
